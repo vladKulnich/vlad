7398710c640ef4d801f4131da2f248f63b7b8902
vladKulnich <113752929+vladKulnich@users.noreply.github.com>
2022-10-20 23:53:00 +0300

    Add files via upload

---
+++ 10(1).py
@@ -0,0 +1,23 @@
+x='sentences.txt'
+f=open(x,"r") 
+words=0
+crapka=0
+okluk=0
+pitanna=0
+for line in f:
+    words += len(line.split())
+    s=list(line)
+    for e in s:
+        if e==".":
+            crapka= crapka+1
+        elif e=="!":
+            okluk=okluk+1
+        elif e=="?":
+            pitanna=pitanna+1
+print("cлів ",words)
+print("розповідних  ",crapka)
+print("окличні ",okluk)
+print("питальні  ",pitanna)
+
+
+
